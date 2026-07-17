9c55b090e57c1b1aa2c5ff448a21817a2bdf41cb
neptumiun <tymothe_billerey@fastmail.fr>
2026-07-17 11:44:28 +0200

    cleanup: remove unused file

--- crates/cli/src/tree_sitter_cli.rs
+++ crates/cli/src/tree_sitter_cli.rs
@@ -4,13 +4,10 @@ pub mod highlight;
 pub mod input;
 pub mod logger;
 pub mod query_testing;
-pub mod tags;
 pub mod test;
 pub mod test_highlight;
 pub mod test_tags;
 pub mod util;
-pub mod version;
-pub mod wasm;
 
 #[cfg(test)]
 mod tests;
